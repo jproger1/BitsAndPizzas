57cb0dc1fa00e8cd38fe848910a5eba2c6d70e06
Jproger <webscripting@ya.ru>
2018-07-31 15:05:15 +0300

    Add pageTitle

--- app/src/main/java/com/jproger/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/jproger/bitsandpizzas/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jproger.bitsandpizzas;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -27,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         ViewPager pager = (ViewPager) findViewById(R.id.pages);
         pager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
@@ -83,6 +88,18 @@ public class MainActivity extends AppCompatActivity {
                     return new PastaFragment();
                 case 3:
                     return new StoresFragment();
+            }
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: return getResources().getText(R.string.home_tab);
+                case 1: return getResources().getText(R.string.pizza_tab);
+                case 2: return getResources().getText(R.string.pasta_tab);
+                case 3: return getResources().getText(R.string.store_tab);
             }
             return null;
         }
